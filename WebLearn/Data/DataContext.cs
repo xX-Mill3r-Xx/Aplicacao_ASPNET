6d9df3687f62b7d36ad4777067c61caa3dd0a4ff
Miller Vasco <millermartinsvasco1@gmail.com>
2022-06-13 21:17:16 -0300

    Initial 2117

---
+++ WebLearn/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebLearn.Models;
+
+namespace WebLearn.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Cadastro> Cadastros { get; set; }
+    }
+}
---
+++ WebLearn/Models/Cadastro.cs
@@ -0,0 +1,9 @@
+﻿namespace WebLearn.Models
+{
+    public class Cadastro
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
