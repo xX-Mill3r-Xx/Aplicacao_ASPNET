19ae18aa9dffb289fb8c5f7da192740c67fe8fe2
Miller Vasco <millermartinsvasco1@gmail.com>
2022-06-15 11:14:54 -0300

    Atualização de conteudo

--- WebLearn/Models/Cadastro.cs
+++ WebLearn/Models/Cadastro.cs
@@ -1,9 +1,15 @@
-﻿namespace WebLearn.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebLearn.Models
 {
     public class Cadastro
     {
         public int Id { get; set; }
+
+        [Display(Name ="Nome")]
         public string Name { get; set; }
+
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
     }
 }
